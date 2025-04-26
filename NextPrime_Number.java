211357fbe3b259a944a8950ea017d6fdc17aa16d
Vidhyaa Thiyagarajan <146201462+Vidhyaa7441@users.noreply.github.com>
2025-04-26 23:52:48 +0530

    Add files via upload

---
+++ NextPrime_Number.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class NextPrime_Number {
+    public static void main(String[] args)
+    {
+        Scanner obj=new Scanner(System.in);
+        int num=obj.nextInt();
+        int next = num + 1;
+        while (true) {
+            if (next > 1) {
+                int i;
+                for (i = 2; i <= Math.sqrt(next); i++) {
+                    if (next % i == 0) {
+                        break;
+                    }
+                }
+                if (i > Math.sqrt(next)) {
+                    System.out.println("Next prime: " + next);
+                    break;
+                }
+            }
+            next++;
+        }
+    }
+}
+
